c5cb71051e046be85b229ed9ffa16466422a32b2
Vladimir Uspensky <vl.shev@list.ru>
2022-06-14 18:23:21 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from "./components/UI/sidebar/Sidebar";
+import Header from "./components/UI/header/Header";
+import AppRouter from "./components/AppRouter";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return (
+        <div className="app">
+            <Sidebar/>
+            <Header/>
+            <div className='content'>
+                <AppRouter/>
+            </div>
+        </div>
   );
 }
 
